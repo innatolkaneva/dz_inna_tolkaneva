eb05daca232f0e4d8317312b82585573ab8894c0
Inna Tolkaneva <innaefimova93@gmail.com>
2024-05-02 10:07:36 +0300

    wright function create array and print array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string [] CreateArrayStr(int len)
+{  string [] listStr = new string[len];
+    for (int i = 0; i < len; i++)
+{
+    listStr[i] = Console.ReadLine();
+}
+return listStr;
+}
+string [] listStr = CreateArrayStr(3);
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+PrintArray(listStr);
