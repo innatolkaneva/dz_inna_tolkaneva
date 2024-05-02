cc73c0c9e6e8f91e0ee22f3029b463e1430e86ee
Inna Tolkaneva <innaefimova93@gmail.com>
2024-05-02 10:45:13 +0300

    Finish wright programm

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿
+
+using System.Collections.Specialized;
+
 string [] CreateArrayStr(int len)
 {  string [] listStr = new string[len];
     for (int i = 0; i < len; i++)
@@ -7,7 +10,7 @@ string [] CreateArrayStr(int len)
 }
 return listStr;
 }
-string [] listStr = CreateArrayStr(3);
+string [] listStr = CreateArrayStr(5);
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -17,3 +20,39 @@ void PrintArray(string[] array)
     System.Console.WriteLine();
 }
 PrintArray(listStr);
+int CountCharString(string str)
+{
+  int counter = 0;
+  foreach (char item in str)
+  {
+      counter ++;
+    }
+return counter;
+}
+int CountWords(string[] listStr)
+{  int counter2 = 0;
+    foreach (string item in listStr)
+    {
+        if (CountCharString(item) <= 3)
+        {
+            counter2++;
+        }
+    }
+    return counter2;
+}
+string [] CreateNewArrayStr(int len)
+{  string [] listNewStr = new string[len];
+int i = 0;
+     foreach (string item in listStr)
+
+    {
+        if (CountCharString(item) <= 3)
+        {
+            listNewStr[i] = item;
+            i++;
+        }
+    }
+return listNewStr;
+}
+string[] newArrayString = CreateNewArrayStr(CountWords(listStr));
+PrintArray(newArrayString);
